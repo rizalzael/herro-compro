d634b7c10ef035b4be9530f7a4abed17a43ece22
Rizal Zaelani <workzael@gmail.com>
2023-10-26 11:24:17 +0700

    update header
    penambahan sub produk HC

--- assets/js/main.js
+++ assets/js/main.js
@@ -265,23 +265,6 @@
    */
   new PureCounter();
 
-      // Dropdown on mouse hover
-      $(document).ready(function () {
-        function toggleNavbarMethod() {
-            if ($(window).width() > 768) {
-                $('.navbar .dropdown').on('mouseover', function () {
-                    $('.dropdown-toggle', this).trigger('click');
-                }).on('mouseout', function () {
-                    $('.dropdown-toggle', this).trigger('click').blur();
-                });
-            } else {
-                $('.navbar .dropdown').off('mouseover').off('mouseout');
-            }
-        }
-        toggleNavbarMethod();
-        $(window).resize(toggleNavbarMethod);
-    });
-    
     
     // Back to top button
     $(window).scroll(function () {
