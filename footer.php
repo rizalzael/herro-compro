e5a9db80f7c1ff5a75c120e8302ea60489a85d19
Rizal Zaelani <workzael@gmail.com>
2023-11-01 18:01:27 +0700

    blog renewal and index fixing
    penambahan page blog page single dan membenahi halaman utama

---
+++ footer.php
@@ -0,0 +1,110 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+
+    <div class="footer-newsletter">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-12 text-center">
+            <h4>Langganan Email Kami!</h4>
+            <p>Dapatkan informasi, berita hingga promo terbaru lebih cepat!</p>
+          </div>
+          <div class="col-lg-6">
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row gy-4">
+          <div class="col-lg-5 col-md-12 footer-info">
+            <a href="index.html" class="logo d-flex align-items-center">
+              <img src="assets/img/logo.png" alt="">
+              <span>PT HDI</span>
+            </a>
+            <p>PT Herro Dynamics Indonesia (HDI) adalah dealer terkemuka HANGCHA Forklift di Indonesia. Dikenal karena layanan berkualitas dan produk inovatif, HDI menawarkan solusi forklift yang handal dan profesional untuk berbagai bisnis. Dengan dukungan purna jual yang responsif, HDI menjadi mitra andal bagi operasional bisnis.</p>
+            <div class="social-links mt-3">
+              <a href="https://www.tiktok.com/@hangcha_id/" class="tiktok"><i class="bi bi-tiktok"></i></a>
+              <a href="https://www.facebook.com/herrodynamicsid/" class="facebook"><i class="bi bi-facebook"></i></a>
+              <a href="https://instagram.com/herrodynamics/" class="instagram"><i class="bi bi-instagram"></i></a>
+              <a href="https://www.linkedin.com/company/herrodynamicsindonesia/" class="linkedin"><i class="bi bi-linkedin"></i></a>
+            </div>
+          </div>
+
+          <div class="col-lg-2 col-6 footer-links">
+            <h4>Menu</h4>
+            <ul>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Beranda</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Tentang</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">HANGCHA</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Blog</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Kotak</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-2 col-6 footer-links">
+            <h4>HANGCHA</h4>
+            <ul>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Official Web</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Produk</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Purna Jual</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Sales Locator</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="#">Tentang HANGCHA</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+            <h4>Hubungi Kami</h4>
+            <p>
+              THC 59 Kamal Muara Penjaringan <br>
+              Jakarta Utara, DKI Jakarta<br>
+              Indonesia <br><br>
+              <strong>Phone:</strong> +62 212 2574 351<br>
+              <strong>WhatsApp:</strong> +62 822 9888 9558<br>
+              <strong>Email:</strong> cs@herro.co.id<br>
+            </p>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>PT Herro Dynamics Indonesia</span></strong>. All Rights Reserved
+        <script>
+          document.write(new Date().getFullYear());
+        </script>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div style="position: fixed; bottom: 70px; right: 30px;">
+    <a href="https://wa.me/6282298889558" target="_blank" style="display: flex; align-items: center; justify-content: center; width: 60px; height: 60px; background-color: green; color: white; border-radius: 50%; box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.2); text-decoration: none;">
+      <i style="font-size: 24px;" class="bi bi-whatsapp"></i>
+    </a>
+  </div>
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Dealer Forklift Terpercaya | Jual, Sewa, dan Layanan Forklift Terbaik</title>
+  <meta content="Selamat datang di dealer forklift terkemuka kami. Kami menawarkan jual, sewa, dan layanan forklift terbaik dengan berbagai pilihan model dan merek terkenal. Hubungi kami hari ini untuk solusi forklift yang tepat untuk bisnis Anda." name="description">
+  <meta content="dealer forklift, jual forklift, sewa forklift, layanan forklift, forklift terbaik, harga forklift, forklift baru, forklift bekas, dealer forklift terpercaya, merek forklift terkenal, solusi forklift bisnis, forklift elektrik, forklift diesel, forklift gas, servis forklift, suku cadang forklift, perawatan forklift, forklift industri, forklift gudang, forklift konstruksi" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NLCND23J');</script>
+        <!-- End Google Tag Manager -->
+
+</head>
+
+<body>
+  <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NLCND23J"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center">
+        <img src="assets/img/Herro Logo.png" alt="logo pt herro dynamics indonesia">
+        <span></span>
+      </a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Beranda</a></li>
+          <li><a class="nav-link scrollto" href="#about">Tentang</a></li>
+
+
+          <li class="dropdown"><a href="#"><span>HANGCHA</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="htps://www.hangcha.co.id" target="_blank">Official Web</a></li>
+              <li class="dropdown"><a href="#"><span>Produk</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="produk/forkliftdiesel.html">Internal Combustion/Diesel</a></li>
+                  <li><a href="produk/electricleadacid.html">Electric Lead-Acid</a></li>
+                  <li><a href="produk/electriclithium.html">Electric Lithium</a></li>
+                  <li><a href="produk/highvoltage.html">High Voltage Li-Ion Counterbalance</a></li>
+                  <li><a href="produk/palletstacker.html">Pallet Stacker</a></li>
+                  <li><a href="produk/pallettruck.html">Pallet Truck</a></li>
+                  <li><a href="produk/orderpicker.html">Order Picker</a></li>
+                  <li><a href="produk/reachtruck.html">Reach Truck</a></li>
+                  <li><a href="produk/towtractor.html">Tow Tractor</a></li>
+                  <li><a href="produk/aerialplatform.html">Aerial Platform</a></li>
+                  <li><a href="produk/roughterrain.html">Rough Terrrain</a></li>
+                </ul>
+              </li>
+              <li><a href="#services">Purna Jual</a></li>
+              <li><a href="saleslocator.html">Sales Locator</a></li>
+              <li><a href="tentangHC.html">Tentang HANGCHA</a></li>
+            </ul>
+          </li>
+          <li><a href="blog.html">Blog</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Kontak</a></li>
+          <li><a class="getstarted scrollto" href="getquote.html">Minta Penawaran</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
