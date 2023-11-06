26074c39cc2dbc5817a33de27ee3058a98380edb
Rizal Zaelani <workzael@gmail.com>
2023-11-06 17:34:27 +0700

    uptd slider

--- assets/js/sisajs.js
+++ assets/js/sisajs.js
@@ -1,3 +1,35 @@
+  /**
+   * Testimonials slider
+   */
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 40
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      1200: {
+        slidesPerView: 4,
+      }
+    }
+  });
+
+
 
   /*--------------------------------------------------------------
 # Counts
