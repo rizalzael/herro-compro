7fc34eb95d31482a758c4fb07dc1a70bae5bb17a
Azharrr <muhammadazharrosadii@gmail.com>
2024-02-05 14:58:32 +0700

    get quote kelar

---
+++ forms/getquote.php
@@ -0,0 +1,42 @@
+<?php
+if(isset($_POST['email'])) {
+
+    // Ganti email penerima dengan email yang sesuai
+    $email_to = "cs@herro.co.id";
+    $email_subject = "Get Quote";
+
+    function died($error) {
+        echo "Maaf, tetapi terdapat kesalahan dalam formulir yang Anda kirim.";
+        die();
+    }
+
+    // validasi data
+    if(!isset($_POST['name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['subject']) ||
+        !isset($_POST['message'])) {
+        died('Maaf, terdapat masalah dengan formulir yang Anda kirim.');       
+    }
+
+    $name = $_POST['name']; // diambil dari formulir
+    $email_from = $_POST['email']; // diambil dari formulir
+    $subject = $_POST['subject']; // diambil dari formulir
+    $message = $_POST['message']; // diambil dari formulir
+
+    $email_message = "Detail formulir kontak:\n\n";
+    $email_message .= "Nama: ".$name."\n";
+    $email_message .= "Email: ".$email_from."\n";
+    $email_message .= "Subject: ".$subject."\n";
+    $email_message .= "Pesan: ".$message."\n";
+
+    // create email headers
+    $headers = 'From: '.$email_from."\r\n".
+    'Reply-To: '.$email_from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);  
+?>
+<!-- include your own success html here -->
+Terima kasih telah menghubungi kami. Kami akan segera menghubungi Anda kembali.
+<?php
+}
+?>
